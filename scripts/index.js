6808e4d225a62c32ed6e05a09e759a471803626d
EMAPHD <42302324+lmalexander@users.noreply.github.com>
2024-07-14 04:23:40 -0400

    added basic papaparse.js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,2 +1,15 @@
 console.log("index.js is connected")
 
+// ********** DATA RETURN
+// > papaparse CSV to JSON pull
+Papa.parse(mbldcGSheetURL, {
+    download: true,
+    header: true,
+    complete: showData,
+});
+
+// > MBLDC data return function 
+ function showData(result) {
+
+
+    }
