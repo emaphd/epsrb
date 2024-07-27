66801e7913065d4087c120b37be97ebfe9ad7baf
EMAPHD <42302324+lmalexander@users.noreply.github.com>
2024-07-27 15:59:42 -0400

    project card + modal return troubleshoot 4

--- scripts/index.js
+++ scripts/index.js
@@ -24,12 +24,15 @@ Papa.parse(emaphdGSheetURL, {
 
     for (let index = 0; index < rawData.length; index++) {
 
+        console.log(rawData);
+/* 
         let isProject = function(rawData) {
             return rawData[index].projEventSelect === "Project";
         };
         emaphdDataObj.project = rawData.filter(isProject);
         console.log(emaphdDataObj.project);
         console.log(emaphdDataObj);
+ */        
 
 /* 
         if (rawData[index].projEventSelect === "Project") {
